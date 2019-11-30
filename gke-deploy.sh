3a5c8bfe7be64f4d7a0065b7f9c2744f746b0343
Kehinde Adewusi <kehinde.adewusi@evincesystems.com>
2019-11-30 21:35:28 +0000

    with travis

---
+++ gke-deploy.sh
@@ -0,0 +1,17 @@
+docker build -t kehindeadewusi/mclient:lastest -t kehindeadewusi/mclient:$SHA -f ./client/Dockerfile ./client
+docker build -t kehindeadewusi/mserver:lastest -t kehindeadewusi/mserver:$SHA -f ./server/Dockerfile ./server
+docker build -t kehindeadewusi/mworker:lastest -t kehindeadewusi/mworker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kehindeadewusi/mclient:latest
+docker push kehindeadewusi/mserver:latest
+docker push kehindeadewusi/mworker:latest
+
+docker push kehindeadewusi/mclient:$SHA
+docker push kehindeadewusi/mserver:$SHA
+docker push kehindeadewusi/mworker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=kehindeadewusi/mclient:$SHA
+kubectl set image deployments/server-deployment server=kehindeadewusi/mserver:$SHA
+kubectl set image deployments/worker-deployment worker=kehindeadewusi/mworker:$SHA
